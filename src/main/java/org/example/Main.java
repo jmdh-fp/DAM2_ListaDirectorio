d921d76c4de91acae1284e2d7e6ca5d9a1a90ae8
jm-upmd <43405929+jm-upmd@users.noreply.github.com>
2023-10-01 17:06:26 +0200

    commit inicial

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        String rutaInicial;
+
+        // Si se pasa como parametro la ruta la usa sino la ruta en que nos encontramos.
+        rutaInicial = args.length >= 1 ? args[0] : ".";
+        File fichero = new File(rutaInicial);
+
+        // Existe la ruta
+        if(fichero.exists()) {
+            if(fichero.isFile()) {
+                System.out.println(fichero.getName() + "es un fichero");
+            } else {
+                // Se trata de un directorio
+                System.out.println("Es un directorio: " + fichero.getCanonicalPath());
+                File[] contenidoRuta = fichero.listFiles();
+                for(File ff: contenidoRuta) {
+                    String infoFichero = ff.isFile() ? "[ ]" : "[/]";
+                    System.out.printf("%6s %s\n", infoFichero, ff.getName());
+                }
+            }
+        } else {
+            System.out.println("La ruta no existe: " + fichero.getAbsolutePath());
+            System.exit(0);
+        }
     }
 }
